dc85d567ded267c9cede08b3689d22ece2a62da0
RicardoPimentel2020 <ricardopimentel27@gmail.com>
2020-10-30 16:10:52 -0300

    findById Implemmentation 5

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,21 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		/*
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(21, "Rick", "ricardo@gmail.com" , new Date(),  3000.0, obj);
+		*/
 		
-		SellerDao sellerdao = DaoFactory.createSellerDao();
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.finById(3);
 		
 		System.out.println(seller);
 		
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,17 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Department id);
+	Department finById(Integer id);
+	List<Department> findAll();
+	
+	
 
 }
